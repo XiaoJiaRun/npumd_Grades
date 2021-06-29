caad0b458368915d2f9f0333cf6cff43b849bbcb
JiaLess-go <67527286+JiaLess-go@users.noreply.github.com>
2021-06-29 10:49:04 +0800

    6.29

--- src/npumd/Verification_code.java
+++ src/npumd/Verification_code.java
@@ -5,15 +5,42 @@ import javax.imageio.stream.ImageInputStream;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Verification_code {
         public Verification_code() throws IOException {
+
+            String strurl="http://urp.mdit.edu.cn/loginAction.do";
+            String Login_html="";
+            String regStr="/validateCodeAction.do.random=...................";
+            try {
+                URL url=new URL(strurl);
+                URLConnection conn=url.openConnection();
+                java.io.InputStream is=conn.getInputStream();
+                BufferedReader br=new BufferedReader(new InputStreamReader(is));
+                String line=null;
+                while((line=br.readLine())!=null){
+                    Login_html+=line;
+                }
+                System.out.println(Login_html);
+                Pattern pattern=Pattern.compile(regStr);
+                Matcher matcher=pattern.matcher(Login_html);
+                while(matcher.find()) {
+                    System.out.println(matcher.group());
+                }
+                br.close();
+            } catch (Exception e) {
+
+                e.printStackTrace();
+            }
+
+
+
             String NPUMD="http://urp.mdit.edu.cn//validateCodeAction.do?random=0.42101651514554983";
             URL url=new URL(NPUMD);
             HttpURLConnection urlConnection=(HttpURLConnection) url.openConnection();
@@ -42,19 +69,20 @@ public class Verification_code {
             frame.setVisible(true);
 
         }
-        class Panel extends JPanel{
-            Image img;
-            public Panel(Image img){
-                this.img = img;
-                repaint();
-            }
-            protected void paintComponent(Graphics g) {
 
-                super.paintComponent(g);
-                g.drawImage(img,0 ,0 , getWidth(), getHeight(), this);
 
-            }
-    }
+}
+    class Panel extends JPanel{
+        Image img;
+        public Panel(Image img){
+            this.img = img;
+            repaint();
+        }
+        protected void paintComponent(Graphics g) {
+
+            super.paintComponent(g);
+            g.drawImage(img,0 ,0 , getWidth(), getHeight(), this);
+        }
 
 }
 
